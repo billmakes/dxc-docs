152f92c91ca030f4cec1e4ec97544d3abeae974d
Bill Hilbert <hilbertwilliam@gmail.com>
2019-08-09 13:55:47 -0400

    sadsad

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "DXC Project Documentation",
   // dest: "../../public",
-  base: "/",
+  base: "dxc-docs",
   themeConfig: {
     logo:
       "https://msepjobs.militaryonesource.mil/msep-api/images/partner-logos/F4E3FAA3-48DEA016.png",
