ba2a3489a69691266b2f3c401096cea036456843
Bill Hilbert <hilbertwilliam@gmail.com>
2019-08-09 15:20:54 -0400

    code

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,6 @@
 module.exports = {
   title: "DXC Documenation",
   base: "/",
-  plugins: [
-    [
-      "component-docgen",
-      {
-        rootDir: "../../src/core"
-      }
-    ]
-  ],
-  postcss: {
-    plugins: [require("autoprefixer")]
-  },
   themeConfig: {
     logo: "https://www.katacoda.com/dxc/avatar",
     nav: [{ text: "Home", link: "/" }, { text: "Guide", link: "/lux/guide/" }],
